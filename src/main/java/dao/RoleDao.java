e500d7873c471c6c12b82a28c6abb423689c5752
Hummer <Hummer@Raven>
2015-06-05 12:47:47 +0300

    Update RoleDao

--- src/main/java/dao/RoleDao.java
+++ src/main/java/dao/RoleDao.java
@@ -1,7 +1,14 @@
 package dao;
 
+import java.util.List;
+
 import table.Role;
+import table.User;
 
 public interface RoleDao extends MainDao<Role, Integer>{
-	
+		public List<Role> getRolesOfUser(Integer userId);
+		
+		public List<User> getUsersByRole(String roleTitle);
+		
+		
 }
--- src/main/java/dao/daoImpl/RoleDaoImpl.java
+++ src/main/java/dao/daoImpl/RoleDaoImpl.java
@@ -2,7 +2,12 @@ package dao.daoImpl;
 
 import java.util.List;
 
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
 import table.Role;
+import table.User;
+import util.HibernateUtil;
 import dao.MainDao;
 import dao.RoleDao;
 
@@ -10,6 +15,40 @@ public class RoleDaoImpl extends MainDaoImpl<Role, Integer> implements RoleDao{
 
 	public RoleDaoImpl() {
 		super(Role.class);
+	}
+
+	public List<Role> getRolesOfUser(Integer userId) {
+		Session session = null;
+		List<Role> roles;
+		try{
+			session = HibernateUtil.getSessionFactory().openSession();
+			Transaction transaction = session.beginTransaction();
+			roles = (List<Role>)session.createSQLQuery("Select * from Role as r Where r.users.id=:id").setParameter("id",userId).list();
+			transaction.commit();
+			
+		}finally{
+			if ((session != null) && (session.isOpen())) {
+				
+			}
+		}
+		return roles;
+	}
+
+	public List<User> getUsersByRole(String roleTitle) {
+		Session session = null;
+		List<User> users;
+		try{
+			session = HibernateUtil.getSessionFactory().openSession();
+			Transaction transaction = session.beginTransaction();
+			users = (List<User>)session.createSQLQuery("Select * from User as u Join Role as r ON u.id=r.users.id Where r.roleTitle=:roleTitle").setParameter("roleTitle",roleTitle).list();
+			transaction.commit();
+			
+		}finally{
+			if ((session != null) && (session.isOpen())) {
+				
+			}
+		}
+		return users;
 	}
 	
 
